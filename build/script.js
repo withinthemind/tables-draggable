1a366dafd7db6d2a0ddbaa263312dd9e5a09133d
Alexandros Ioannidis <alex@withinthemind.gr>
2021-03-28 12:47:25 +0300

    clean up

--- build/script.js
+++ build/script.js
@@ -11,68 +11,28 @@ class App extends React.Component {
     }
   };
 
-  handleDrag = (e, ui) => {
-    const {x, y} = this.state.deltaPosition;
-    this.setState({
-      deltaPosition: {
-        x: x + ui.deltaX,
-        y: y + ui.deltaY,
-      }
-    });
-  };
-
-  // For controlled component
-  adjustXPos = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    const {x, y} = this.state.controlledPosition;
-    this.setState({controlledPosition: {x: x - 10, y}});
-  };
-
-  adjustYPos = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    const {controlledPosition} = this.state;
-    const {x, y} = controlledPosition;
-    this.setState({controlledPosition: {x, y: y - 10}});
-  };
-
-  onControlledDrag = (e, position) => {
-    const {x, y} = position;
-    this.setState({controlledPosition: {x, y}});
-  };
-
-  onControlledDragStop = (e, position) => {
-    this.onControlledDrag(e, position);
-    this.onStop();
-  };
-
   render() {
-    const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
-    const {deltaPosition, controlledPosition} = this.state;
     return (
 	<div>
       <div className="top">
         <h1>Tables Draggable</h1>
-        <Draggable {...dragHandlers}>
+        <Draggable>
           <img src="images/table-1.png" />
         </Draggable>
-        <Draggable {...dragHandlers}>
+        <Draggable>
           <img src="images/table-2.png" />
         </Draggable>
-        <Draggable {...dragHandlers}>
+        <Draggable>
           <img src="images/table-3.png" />
         </Draggable>
-        <Draggable {...dragHandlers}>
+        <Draggable>
           <img src="images/table-4.png" />
         </Draggable>
-        <Draggable {...dragHandlers}>
+        <Draggable>
           <img src="images/table-5.png" />
         </Draggable>		
       </div>
-	  <div className="sala">
-	  
-	  </div>
+	  <div className="sala"></div>
 	</div>
     );
   }
